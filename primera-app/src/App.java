4a337a61729696d6b4da5dc72395f22952a6956d
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-11-06 23:40:42 -0500

    initial commit

---
+++ primera-app/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hola Mundo! Tienes que creer en lo que haces para obtener lo que quieres");
+
+                 System.out.println("Hola Mundo! Tienes que creer en lo que haces para obtener lo que quieres");
+    }
+}
